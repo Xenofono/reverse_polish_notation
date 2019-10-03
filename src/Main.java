a9f223eddfcff529f2a2536d3d47c36a2f1ac30b
Kristoffer Näsström <kristoffer.nasstrom@gmail.com>
2019-10-03 22:30:50 +0200

    some variables renamed for greater clarity and some tricky areas are now commented

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         Polish polish = new Polish("((15÷(7−(1+1)))×3)−(2+(1+1))");
-        System.out.println(polish.getExpressionAsList());
-        System.out.println(polish.calculateExpression());
+        System.out.println(polish.getPostfixExpressionAsList());
+        System.out.println(polish.getSum());
 
 
-        Polish polish2 = new Polish("((35+8)-20*(4/6))+3");
-        System.out.println(polish2.getExpressionAsList());
-        System.out.println(polish2.calculateExpression());
+        Polish polish2 = new Polish("(((35+8)-20*(4/6))+3)");
+        System.out.println(polish2.getPostfixExpressionAsList());
+        System.out.println(polish2.getSum());
 
 
+        Polish polish3 = new Polish("(3 + 5) * (7 – 2)");
+        System.out.println(polish3.getPostfixExpressionAsList());
+        System.out.println(polish3.getSum());
     }
 
 
